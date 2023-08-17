af2e0835de98567559973bc48fc03ab090c0da76
Francesco <fr.bambina@gmail.com>
2023-08-17 16:08:53 +0200

    Added homepage

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def base():
-    return render_template("base.html")
+def home():
+    return render_template("home.html")
 
 @app.route("/diary")
 def diary():
